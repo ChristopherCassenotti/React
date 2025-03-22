175f4afb006106d92a296eb3780fb13baabc24ff
ChristopherCassenotti <craftplays10a@gmail.com>
2025-03-22 12:02:01 -0300

    Dia 2 React

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,69 @@
 import { useState } from "react";
 import AddTask from "./componetes/AddTask";
-import Tasks from "./componetes/Tasks";
+import Task from "./componetes/Task";
 
 function App() {
-    const [tasks, setTasks] = useState([{
-        id: 1,
-        title: "Estudar programação",
-        description: "Estudar programação para se tornar um dev full stack.",
-        isCompleted: false
+  const [task, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar programação para se tornar um dev full stack.",
+      isCompleted: false,
     },
     {
-        id: 2,
-        title: "Estudar inglês",
-        description: "Estudar inglês para se tornar fluente.",
-        isCompleted: false
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar inglês para se tornar fluente.",
+      isCompleted: false,
     },
     {
-        id: 3,
-        title: "Estudar matemática",
-        description: "Estudar matemática porque é a melhor matéria.",
-        isCompleted: false
+      id: 3,
+      title: "Estudar matemática",
+      description: "Estudar matemática porque é a melhor matéria.",
+      isCompleted: false,
     },
-    ]);
+  ]);
 
-    function onTaskClick(tasksId){
-        const newTasks = tasks.map((tasks) => {
-            if(tasks.id === tasksId){
-                return {...tasks, isCompleted: !tasks.isCompleted}
-            }
-            return tasks;
-        })
-        setTasks(newTasks);
-    }
+  function onTaskClick(taskId) {
+    const newTask = task.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTask);
+  }
 
-    function onDeleteTaskClick(tasksId){
-        const deleteTasks = tasks.filter(tasks => tasksId !== tasks.id);
-        setTasks(deleteTasks);
-    }
+  function onDeleteTaskClick(taskId) {
+    const deleteTasks = task.filter((task) => taskId !== task.id);
+    setTasks(deleteTasks);
+  }
 
-    function onAddTaskSubmit (title, description) {
-        const newTask = {
-            id: tasks.length + 2,
-            title,
-            description,
-            isCompleted: false
-        };
-        setTasks([...tasks, newTask]);
-    }
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: task.length + 2,
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...task, newTask]);
+  }
 
-    return (
-        <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-            <div className="w-[500px] space-y-4">
-                <h1 className="text-3xl text-slate-100 font-bold text-center">
-                    Gerenciador de Tarefas
-                </h1>
-                <AddTask onAddTaskSubmit={onAddTaskSubmit} />
-                <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Task
+          task={task}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/componetes/AddTask.jsx
+++ src/componetes/AddTask.jsx
@@ -1,27 +1,43 @@
 import { useState } from "react";
 
-function AddTask({onAddTaskSubmit}) {
-    
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    return(
-        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-            <input value={title} onChange={() => setTitle(event.target.value)} type="text" placeholder="Digite o título da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"/>
-            <input value={description} onChange={() => setDescription(event.target.value)} type="text" placeholder="Digite a descrição da tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"/>
-            <button onClick={() => {
-                if(!title.trim() || !description.trim()){
-                    return alert("Preencha os campos");
-                }else{
-                    return(
-                        onAddTaskSubmit(title, description),
-                        setTitle(""),
-                        setDescription("")
-                    )
-                }   
-            }} className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
-        </div>
-    )
- }
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        value={title}
+        onChange={() => setTitle(event.target.value)}
+        type="text"
+        placeholder="Digite o título da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <input
+        value={description}
+        onChange={() => setDescription(event.target.value)}
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <button
+        onClick={() => {
+          if (!title.trim() || !description.trim()) {
+            return alert("Preencha os campos");
+          } else {
+            return (
+              onAddTaskSubmit(title, description),
+              setTitle(""),
+              setDescription("")
+            );
+          }
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
 
 export default AddTask;
