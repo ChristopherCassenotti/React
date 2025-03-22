a891224d6a6ac3f731e2a1a1e741ea874b8abd66
ChristopherCassenotti <craftplays10a@gmail.com>
2025-03-22 18:30:01 -0300

    Dia 2 React

---
+++ LoginCadastro/telaAcesso/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import BoxLogin from "./componentes/boxLogin.jsx";
+
+function App() {
+  return (
+    <div className="">
+      <BoxLogin />
+    </div>
+  );
+}
+
+export default App;
