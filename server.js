0b435fcf32baf85f7fb0ca6e9017ee52ad8edc39
ChristopherCassenotti <craftplays10a@gmail.com>
2025-03-24 17:34:05 -0300

    BackEnd

--- server.js
+++ server.js
@@ -1,61 +1,45 @@
-import express, { response } from "express";
+import express, { json, response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
+app.get("/usuarios", async (request, response) => {
+  try {
+    let users = [];
+    if (request.query) {
+      users = await prisma.user.findMany({
+        where: {
+          name: request.query.name,
+          email: request.query.email,
+          age: request.query.age,
+        },
+      });
+
+      response.status(200).json(users);
+    } else {
+      const users = await prisma.user.findMany();
+    }
+  } catch {
+    console.log(console.error());
+    response.status(400).json({ error: "Erro no get" });
+  }
+});
+
 app.post("/usuarios", async (request, response) => {
   try {
     const user = await prisma.user.create({
       data: {
-        email: request.body.email,
         name: request.body.name,
+        email: request.body.email,
         age: request.body.age,
       },
     });
-
-    response.status(201).json(user);
-  } catch (error) {
-    console.error(error);
-    response.status(400).json({ error: "Erro ao criar usuário" });
-  }
-});
-
-app.get("/usuarios", async (request, response) => {
-  let users = [];
-  if (request.query) {
-    users = await prisma.user.findMany({
-      where: {
-        name: request.query.name,
-        email: request.query.email,
-        age: request.query.age,
-      },
-    });
-  } else {
-    const users = await prisma.user.findMany();
-  }
-
-  response.status(200).json(users);
-});
-
-app.put("/usuarios/:id", async (request, response) => {
-  try {
-    const user = await prisma.user.update({
-      where: {
-        id: request.params.id,
-      },
-      data: {
-        email: request.body.email,
-        name: request.body.name,
-        age: request.body.age,
-      },
-    });
-
     response.status(200).json(user);
-  } catch (error) {
-    console.error(error);
-    response.status(400).json({ error: "Erro ao atualizar usuário" });
+  } catch {
+    console.log(error);
+    response.status(400).json({ error: "Cadastro não executado" });
   }
 });
 
@@ -66,10 +50,31 @@ app.delete("/usuarios/:id", async (request, response) => {
         id: request.params.id,
       },
     });
-    response.status(200).json({ message: "Usuário deletado com Sucesso!" });
-  } catch (error) {
-    console.error(error);
-    response.status(400).json({ error: "Erro ao atualizar usuário" });
+
+    response.status(200).json({ message: "User deletado" });
+  } catch {
+    console.log(error);
+    response.status(400).json({ error: "User não deletado" });
+  }
+});
+
+app.put("/usuarios/:id", async (request, response) => {
+  try {
+    await prisma.user.update({
+      where: {
+        id: request.params.id,
+      },
+      data: {
+        name: request.body.name,
+        email: request.body.email,
+        age: request.body.age,
+      },
+    });
+
+    response.status(200).json({ message: "User atualizado" });
+  } catch {
+    console.log(error);
+    response.status(400).json({ error: "User não atualizado" });
   }
 });
 
